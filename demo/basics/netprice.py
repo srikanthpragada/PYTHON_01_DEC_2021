263aea07df231c2ebce5d97e852b9ee3dcf91b9c
srikanthpragada <srikanthpragada@gmail.com>
2021-12-07 19:59:30 +0530

    07-DEC

---
+++ demo/basics/netprice.py
@@ -0,0 +1,13 @@
+# Calculate net price
+
+price = float(input("Enter Price :"))
+discount = price * 0.10
+net_price = price - discount
+
+print(f"Price       :   {price:8.2f}")
+print(f"-Discount   :   {discount:8.2f}")
+print(f"Net Price   :   {net_price:8.2f}")
+
+
+
+
